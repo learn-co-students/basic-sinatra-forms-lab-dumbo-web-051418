e72ddd22f00ab73acbdae5b3d626b8dd1fc3cec6
Anthony Lam <al3101@nyu.edu>
2018-06-05 03:33:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  
+  get '/newteam' do 
+    erb :newteam
+  end 
+  
+  post '/newteam' do #make post request on newteam 
+    #Allows the controller to use these instance variables to display information to user when called in erb file
+    @name = params[:name] 
+    @coach = params[:coach] 
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf] 
+    @sf = params[:sf] 
+    @c = params[:c] 
+    erb :team #returns the views of team.erb 
+  end
 
 end
